99f0e3ca5728ef4f1e8a7b84f87347434ab52a50
Daniel Soares <soares.dandansss@gmail.com>
2025-02-18 20:19:25 -0300

    commit

---
+++ aula02/Caneta.php
@@ -0,0 +1,27 @@
+<?php
+
+    class Caneta {
+        var $modelo;
+        var $cor;
+        var $ponta;
+        var $carga;
+        var $tampada;
+
+        function rabiscar() {
+            if ($this->tampada == true) {
+                echo "<p>Erro! Não posso rabiscar!</p>";
+            } else {
+                echo "<p>Estou rabiscando...</p>";
+            }
+        }
+
+        function tampar() {
+            $this->tampada = true;
+        }
+
+        function destampar() {
+            $this->tampada = false;
+        }
+    }
+
+?>
---
+++ aula02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02 - POO</title>
+</head>
+<body>
+    <?php
+        require_once 'Caneta.php';
+
+        $c1 = new Caneta;
+        $c1->ponta = 0.5;
+        $c1->tampada = false;
+        $c1->cor = "Azul";
+        
+        $c1->tampar();
+        $c1->rabiscar();
+
+        //print_r($c1);
+    ?>
+</body>
+</html>
